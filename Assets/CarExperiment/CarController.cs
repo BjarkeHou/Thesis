a533fc69f1ed03f1c673173824783a216a7ad963
Bjarke Hou Kammersgaard <bjarkehou@Bjarkes-MacBook-Pro.local>
2016-11-03 14:02:41 +0100

    Changed from UnityNEAT project

--- Assets/CarExperiment/CarController.cs
+++ Assets/CarExperiment/CarController.cs
@@ -2,194 +2,185 @@
 using System.Collections;
 using SharpNeat.Phenomes;
 
-public class CarController : UnitController {
+public class CarController : UnitController
+{
 
-    public float Speed = 5f;
-    public float TurnSpeed = 180f;
-    public int Lap = 1;
-    public int CurrentPiece, LastPiece;
-    bool MovingForward = true;
-    bool IsRunning;
-    public float SensorRange = 10;
-    int WallHits; 
-    IBlackBox box;
+	public float Speed = 5f;
+	public float TurnSpeed = 180f;
+	public int Lap = 1;
+	public int CurrentPiece, LastPiece;
+	bool MovingForward = true;
+	bool IsRunning;
+	public float SensorRange = 10;
+	int WallHits;
+	IBlackBox box;
+
+	// WEATHER CONDITIONS VARIABLES
+	[Range (0.0f, 1.0f)]
+	public float rain = 0.5f;
+	private float oldTurnAngle = -1;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		oldTurnAngle = this.transform.forward.y;
 	}
 	
 	// Update is called once per frame
-    void FixedUpdate()
-    {
-        //grab the input axes
-        //var steer = Input.GetAxis("Horizontal");
-        //var gas = Input.GetAxis("Vertical");
+	void FixedUpdate ()
+	{
+		//grab the input axes
+		//var steer = Input.GetAxis("Horizontal");
+		//var gas = Input.GetAxis("Vertical");
 
-        ////if they're hittin' the gas...
-        //if (gas != 0)
-        //{
-        //    //take the throttle level (with keyboard, generally +1 if up, -1 if down)
-        //    //  and multiply by speed and the timestep to get the distance moved this frame
-        //    var moveDist = gas * speed * Time.deltaTime;
+		////if they're hittin' the gas...
+		//if (gas != 0)
+		//{
+		//    //take the throttle level (with keyboard, generally +1 if up, -1 if down)
+		//    //  and multiply by speed and the timestep to get the distance moved this frame
+		//    var moveDist = gas * speed * Time.deltaTime;
 
-        //    //now the turn amount, similar drill, just turnSpeed instead of speed
-        //    //   we multiply in gas as well, which properly reverses the steering when going 
-        //    //   backwards, and scales the turn amount with the speed
-        //    var turnAngle = steer * turnSpeed * Time.deltaTime * gas;
+		//    //now the turn amount, similar drill, just turnSpeed instead of speed
+		//    //   we multiply in gas as well, which properly reverses the steering when going 
+		//    //   backwards, and scales the turn amount with the speed
+		//    var turnAngle = steer * turnSpeed * Time.deltaTime * gas;
 
-        //    //now apply 'em, starting with the turn           
-        //    transform.Rotate(0, turnAngle, 0);
+		//    //now apply 'em, starting with the turn           
+		//    transform.Rotate(0, turnAngle, 0);
 
-        //    //and now move forward by moveVect
-        //    transform.Translate(Vector3.forward * moveDist);
-        //}
+		//    //and now move forward by moveVect
+		//    transform.Translate(Vector3.forward * moveDist);
+		//}
 
-        // Five sensors: Front, left front, left, right front, right
+		// Five sensors: Front, left front, left, right front, right
+	
+		if (IsRunning) {
+			float frontSensor = 0;
+			float leftFrontSensor = 0;
+			float leftSensor = 0;
+			float rightFrontSensor = 0;
+			float rightSensor = 0;
 
-        if (IsRunning)
-        {
-            float frontSensor = 0;
-            float leftFrontSensor = 0;
-            float leftSensor = 0;
-            float rightFrontSensor = 0;
-            float rightSensor = 0;
-            // Front sensor
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position + transform.forward * 1.1f, transform.TransformDirection(new Vector3(0, 0, 1).normalized), out hit, SensorRange))
-            {
-                if (hit.collider.tag.Equals("Wall"))
-                {
-                    frontSensor = 1 - hit.distance / SensorRange;
-                }
-            }
 
-            if (Physics.Raycast(transform.position + transform.forward * 1.1f, transform.TransformDirection(new Vector3(0.5f, 0, 1).normalized), out hit, SensorRange))
-            {
-                if (hit.collider.tag.Equals("Wall"))
-                {
-                    rightFrontSensor = 1 - hit.distance / SensorRange;
-                }
-            }
+			// Front sensor
+			RaycastHit hit;
+			if (Physics.Raycast (transform.position + transform.forward * 1.1f, transform.TransformDirection (new Vector3 (0, 0, 1).normalized), out hit, SensorRange)) {
+				if (hit.collider.tag.Equals ("Wall")) {
+					frontSensor = 1 - hit.distance / SensorRange;
+				}
+			}
 
-            if (Physics.Raycast(transform.position + transform.forward * 1.1f, transform.TransformDirection(new Vector3(1, 0, 0).normalized), out hit, SensorRange))
-            {
-                if (hit.collider.tag.Equals("Wall"))
-                {
-                    rightSensor = 1 - hit.distance / SensorRange;
-                }
-            }
+			if (Physics.Raycast (transform.position + transform.forward * 1.1f, transform.TransformDirection (new Vector3 (0.5f, 0, 1).normalized), out hit, SensorRange)) {
+				if (hit.collider.tag.Equals ("Wall")) {
+					rightFrontSensor = 1 - hit.distance / SensorRange;
+				}
+			}
 
-            if (Physics.Raycast(transform.position + transform.forward * 1.1f, transform.TransformDirection(new Vector3(-0.5f, 0, 1).normalized), out hit, SensorRange))
-            {
-                if (hit.collider.tag.Equals("Wall"))
-                {
-                    leftFrontSensor = 1 - hit.distance / SensorRange;
-                }
-            }
+			if (Physics.Raycast (transform.position + transform.forward * 1.1f, transform.TransformDirection (new Vector3 (1, 0, 0).normalized), out hit, SensorRange)) {
+				if (hit.collider.tag.Equals ("Wall")) {
+					rightSensor = 1 - hit.distance / SensorRange;
+				}
+			}
 
-            if (Physics.Raycast(transform.position + transform.forward * 1.1f, transform.TransformDirection(new Vector3(-1, 0, 0).normalized), out hit, SensorRange))
-            {
-                if (hit.collider.tag.Equals("Wall"))
-                {
-                    leftSensor = 1 - hit.distance / SensorRange;
-                }
-            }
+			if (Physics.Raycast (transform.position + transform.forward * 1.1f, transform.TransformDirection (new Vector3 (-0.5f, 0, 1).normalized), out hit, SensorRange)) {
+				if (hit.collider.tag.Equals ("Wall")) {
+					leftFrontSensor = 1 - hit.distance / SensorRange;
+				}
+			}
 
-            ISignalArray inputArr = box.InputSignalArray;
-            inputArr[0] = frontSensor;
-            inputArr[1] = leftFrontSensor;
-            inputArr[2] = leftSensor;
-            inputArr[3] = rightFrontSensor;
-            inputArr[4] = rightSensor;
+			if (Physics.Raycast (transform.position + transform.forward * 1.1f, transform.TransformDirection (new Vector3 (-1, 0, 0).normalized), out hit, SensorRange)) {
+				if (hit.collider.tag.Equals ("Wall")) {
+					leftSensor = 1 - hit.distance / SensorRange;
+				}
+			}
 
-            box.Activate();
+			ISignalArray inputArr = box.InputSignalArray;
+			inputArr [0] = frontSensor;
+			inputArr [1] = leftFrontSensor;
+			inputArr [2] = leftSensor;
+			inputArr [3] = rightFrontSensor;
+			inputArr [4] = rightSensor;
 
-            ISignalArray outputArr = box.OutputSignalArray;
+			box.Activate ();
 
-            var steer = (float)outputArr[0] * 2 - 1;
-            var gas = (float)outputArr[1] * 2 - 1;
+			ISignalArray outputArr = box.OutputSignalArray;
 
-            var moveDist = gas * Speed * Time.deltaTime;
-            var turnAngle = steer * TurnSpeed * Time.deltaTime * gas;
+			var steer = (float)outputArr [0] * 2 - 1;
+			var gas = (float)outputArr [1] * 2 - 1;
 
-            transform.Rotate(new Vector3(0, turnAngle, 0));
-            transform.Translate(Vector3.forward * moveDist);
-        }
-    }
+			var moveDist = gas * Speed * Time.deltaTime;
+			var turnAngle = steer * TurnSpeed * Time.deltaTime * gas;
 
-    public override void Stop()
-    {
-        this.IsRunning = false;
-    }
+			//transform.Translate (Vector3.forward * moveDist * rain);
+			transform.Rotate (new Vector3 (0, turnAngle, 0));
+//			Debug.Log (turnAngle);
+			//transform.Rotate ((new Vector3 (0, turnAngle, 0) + (rain * new Vector3 (0, oldTurnAngle, 0))));
+			//oldTurnAngle = turnAngle;
+			transform.Translate (Vector3.forward * moveDist);
+		}
+	}
 
-    public override void Activate(IBlackBox box)
-    {
-        this.box = box;
-        this.IsRunning = true;
-    }
+	public override void Stop ()
+	{
+		this.IsRunning = false;
+	}
 
-    public void NewLap()
-    {        
-        if (LastPiece > 2 && MovingForward)
-        {
-            Lap++;            
-        }
-    }
+	public override void Activate (IBlackBox box)
+	{
+		this.box = box;
+		this.IsRunning = true;
+	}
 
-    public override float GetFitness()
-    {
-        if (Lap == 1 && CurrentPiece == 0)
-        {
-            return 0;
-        }
-        int piece = CurrentPiece;
-        if (CurrentPiece == 0)
-        {
-            piece = 17;
-        }
-        float fit = Lap * piece - WallHits * 0.2f;
-      //  print(string.Format("Piece: {0}, Lap: {1}, Fitness: {2}", piece, Lap, fit));
-        if (fit > 0)
-        {
-            return fit;
-        }
-        return 0;
-    }
+	public void NewLap ()
+	{        
+		if (LastPiece > 2 && MovingForward) {
+			Lap++;            
+		}
+	}
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.tag.Equals("Road"))
-        {
-            RoadPiece rp = collision.collider.GetComponent<RoadPiece>();
-          //  print(collision.collider.tag + " " + rp.PieceNumber);
+	public override float GetFitness ()
+	{
+		if (Lap == 1 && CurrentPiece == 0) {
+			return 0;
+		}
+		int piece = CurrentPiece;
+		if (CurrentPiece == 0) {
+			piece = 17;
+		}
+		float fit = Lap * piece - WallHits * 0.2f;
+		//  print(string.Format("Piece: {0}, Lap: {1}, Fitness: {2}", piece, Lap, fit));
+		if (fit > 0) {
+			return fit;
+		}
+		return 0;
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.collider.tag.Equals ("Road")) {
+			RoadPiece rp = collision.collider.GetComponent<RoadPiece> ();
+			//  print(collision.collider.tag + " " + rp.PieceNumber);
             
-            if ((rp.PieceNumber != LastPiece) && (rp.PieceNumber == CurrentPiece + 1 || (MovingForward && rp.PieceNumber == 0)))
-            {
-                LastPiece = CurrentPiece;
-                CurrentPiece = rp.PieceNumber;
-                MovingForward = true;                
-            }
-            else
-            {
-                MovingForward = false;
-            }
-            if (rp.PieceNumber == 0)
-            {
-                CurrentPiece = 0;
-            }
-        }
-        else if (collision.collider.tag.Equals("Wall"))
-        {
-            WallHits++;
-        }
-    }
+			if ((rp.PieceNumber != LastPiece) && (rp.PieceNumber == CurrentPiece + 1 || (MovingForward && rp.PieceNumber == 0))) {
+				LastPiece = CurrentPiece;
+				CurrentPiece = rp.PieceNumber;
+				MovingForward = true;                
+			} else {
+				MovingForward = false;
+			}
+			if (rp.PieceNumber == 0) {
+				CurrentPiece = 0;
+			}
+		} else if (collision.collider.tag.Equals ("Wall")) {
+			WallHits++;
+		}
+	}
 
 
 
-    //void OnGUI()
-    //{
-    //    GUI.Button(new Rect(10, 200, 100, 100), "Forward: " + MovingForward + "\nPiece: " + CurrentPiece + "\nLast: " + LastPiece + "\nLap: " + Lap);
-    //}
+	//void OnGUI()
+	//{
+	//    GUI.Button(new Rect(10, 200, 100, 100), "Forward: " + MovingForward + "\nPiece: " + CurrentPiece + "\nLast: " + LastPiece + "\nLap: " + Lap);
+	//}
     
 }
--- Assets/SharpNEAT/Optimizer.cs
+++ Assets/SharpNEAT/Optimizer.cs
@@ -8,227 +8,219 @@ using System;
 using System.Xml;
 using System.IO;
 
-public class Optimizer : MonoBehaviour {
+public class Optimizer : MonoBehaviour
+{
 
-    const int NUM_INPUTS = 5;
-    const int NUM_OUTPUTS = 2;
+	const int NUM_INPUTS = 5;
+	const int NUM_OUTPUTS = 2;
 
-    public int Trials;
-    public float TrialDuration;
-    public float StoppingFitness;
-    bool EARunning;
-    string popFileSavePath, champFileSavePath;
+	public int Trials;
+	public float TrialDuration;
+	public float StoppingFitness;
+	[Range (0, 20)]
+	public int evoSpeed = 20;
+	public string folder_prefix = "test_1";
+	bool EARunning;
+	string popFileSavePath, champFileSavePath;
 
-    SimpleExperiment experiment;
-    static NeatEvolutionAlgorithm<NeatGenome> _ea;
+	SimpleExperiment experiment;
+	static NeatEvolutionAlgorithm<NeatGenome> _ea;
 
-    public GameObject Unit;
+	public GameObject Unit;
 
-    Dictionary<IBlackBox, UnitController> ControllerMap = new Dictionary<IBlackBox, UnitController>();
-    private DateTime startTime;
-    private float timeLeft;
-    private float accum;
-    private int frames;
-    private float updateInterval = 12;
+	Dictionary<IBlackBox, UnitController> ControllerMap = new Dictionary<IBlackBox, UnitController> ();
+	private DateTime startTime;
+	private float timeLeft;
+	private float accum;
+	private int frames;
+	private float updateInterval = 12;
 
-    private uint Generation;
-    private double Fitness;
+	private uint Generation;
+	private double Fitness;
 
 	// Use this for initialization
-	void Start () {
-        Utility.DebugLog = true;
-        experiment = new SimpleExperiment();
-        XmlDocument xmlConfig = new XmlDocument();
-        TextAsset textAsset = (TextAsset)Resources.Load("experiment.config");
-        xmlConfig.LoadXml(textAsset.text);
-        experiment.SetOptimizer(this);
+	void Start ()
+	{
+		Utility.DebugLog = true;
+		experiment = new SimpleExperiment ();
+		XmlDocument xmlConfig = new XmlDocument ();
+		TextAsset textAsset = (TextAsset)Resources.Load ("experiment.config");
+		xmlConfig.LoadXml (textAsset.text);
+		experiment.SetOptimizer (this);
 
-        experiment.Initialize("Car Experiment", xmlConfig.DocumentElement, NUM_INPUTS, NUM_OUTPUTS);
+		experiment.Initialize ("test1", xmlConfig.DocumentElement, NUM_INPUTS, NUM_OUTPUTS);
 
-        champFileSavePath = Application.persistentDataPath + string.Format("/{0}.champ.xml", "car");
-        popFileSavePath = Application.persistentDataPath + string.Format("/{0}.pop.xml", "car");       
+		champFileSavePath = Application.persistentDataPath + string.Format ("/{1}/{0}.champ.xml", "car", folder_prefix);
+		popFileSavePath = Application.persistentDataPath + string.Format ("/{1}/{0}.pop.xml", "car", folder_prefix);       
 
-        print(champFileSavePath);
+		print (champFileSavePath);
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-      //  evaluationStartTime += Time.deltaTime;
+	// Update is called once per frame
+	void Update ()
+	{
+		Time.timeScale = evoSpeed;       
 
-        timeLeft -= Time.deltaTime;
-        accum += Time.timeScale / Time.deltaTime;
-        ++frames;
+		//  evaluationStartTime += Time.deltaTime;
 
-        if (timeLeft <= 0.0)
-        {
-            var fps = accum / frames;
-            timeLeft = updateInterval;
-            accum = 0.0f;
-            frames = 0;
-            //   print("FPS: " + fps);
-            if (fps < 10)
-            {
-                Time.timeScale = Time.timeScale - 1;
-                print("Lowering time scale to " + Time.timeScale);
-            }
-        }
-    }
+		timeLeft -= Time.deltaTime;
+		accum += Time.timeScale / Time.deltaTime;
+		++frames;
 
-    public void StartEA()
-    {        
-        Utility.DebugLog = true;
-        Utility.Log("Starting PhotoTaxis experiment");
-        // print("Loading: " + popFileLoadPath);
-        _ea = experiment.CreateEvolutionAlgorithm(popFileSavePath);
-        startTime = DateTime.Now;
+		if (timeLeft <= 0.0) {
+			var fps = accum / frames;
+			timeLeft = updateInterval;
+			accum = 0.0f;
+			frames = 0;
+			//   print("FPS: " + fps);
+			if (fps < 10) {
+				evoSpeed = evoSpeed - 1;
+				//Time.timeScale = Time.timeScale - 1;
+				print ("Lowering time scale to " + evoSpeed);
+			}
+		}
+	}
 
-        _ea.UpdateEvent += new EventHandler(ea_UpdateEvent);
-        _ea.PausedEvent += new EventHandler(ea_PauseEvent);
+	public void StartEA ()
+	{        
+		Utility.DebugLog = true;
+		Utility.Log ("Starting PhotoTaxis experiment");
+		// print("Loading: " + popFileLoadPath);
+		_ea = experiment.CreateEvolutionAlgorithm (popFileSavePath);
+		startTime = DateTime.Now;
 
-        var evoSpeed = 25;
+		_ea.UpdateEvent += new EventHandler (ea_UpdateEvent);
+		_ea.PausedEvent += new EventHandler (ea_PauseEvent);
 
-     //   Time.fixedDeltaTime = 0.045f;
-        Time.timeScale = evoSpeed;       
-        _ea.StartContinue();
-        EARunning = true;
-    }
+		//   Time.fixedDeltaTime = 0.045f;
+		_ea.StartContinue ();
+		EARunning = true;
+	}
 
-    void ea_UpdateEvent(object sender, EventArgs e)
-    {
-        Utility.Log(string.Format("gen={0:N0} bestFitness={1:N6}",
-            _ea.CurrentGeneration, _ea.Statistics._maxFitness));
+	void ea_UpdateEvent (object sender, EventArgs e)
+	{
+		Utility.Log (string.Format ("gen={0:N0} bestFitness={1:N6}",
+			_ea.CurrentGeneration, _ea.Statistics._maxFitness));
 
-        Fitness = _ea.Statistics._maxFitness;
-        Generation = _ea.CurrentGeneration;
+		Fitness = _ea.Statistics._maxFitness;
+		Generation = _ea.CurrentGeneration;
       
 
-    //    Utility.Log(string.Format("Moving average: {0}, N: {1}", _ea.Statistics._bestFitnessMA.Mean, _ea.Statistics._bestFitnessMA.Length));
+		//    Utility.Log(string.Format("Moving average: {0}, N: {1}", _ea.Statistics._bestFitnessMA.Mean, _ea.Statistics._bestFitnessMA.Length));
 
     
-    }
+	}
 
-    void ea_PauseEvent(object sender, EventArgs e)
-    {
-        Time.timeScale = 1;
-        Utility.Log("Done ea'ing (and neat'ing)");
+	void ea_PauseEvent (object sender, EventArgs e)
+	{
+		Time.timeScale = 1;
+		Utility.Log ("Done ea'ing (and neat'ing)");
 
-        XmlWriterSettings _xwSettings = new XmlWriterSettings();
-        _xwSettings.Indent = true;
-        // Save genomes to xml file.        
-        DirectoryInfo dirInf = new DirectoryInfo(Application.persistentDataPath);
-        if (!dirInf.Exists)
-        {
-            Debug.Log("Creating subdirectory");
-            dirInf.Create();
-        }
-        using (XmlWriter xw = XmlWriter.Create(popFileSavePath, _xwSettings))
-        {
-            experiment.SavePopulation(xw, _ea.GenomeList);
-        }
-        // Also save the best genome
+		XmlWriterSettings _xwSettings = new XmlWriterSettings ();
+		_xwSettings.Indent = true;
+		// Save genomes to xml file.        
+		DirectoryInfo dirInf = new DirectoryInfo (Application.persistentDataPath + string.Format ("/{0}", folder_prefix));
+		if (!dirInf.Exists) {
+			Debug.Log ("Creating subdirectory");
+			dirInf.Create ();
+		}
+		using (XmlWriter xw = XmlWriter.Create (popFileSavePath, _xwSettings)) {
+			experiment.SavePopulation (xw, _ea.GenomeList);
+		}
+		// Also save the best genome
 
-        using (XmlWriter xw = XmlWriter.Create(champFileSavePath, _xwSettings))
-        {
-            experiment.SavePopulation(xw, new NeatGenome[] { _ea.CurrentChampGenome });
-        }
-        DateTime endTime = DateTime.Now;
-        Utility.Log("Total time elapsed: " + (endTime - startTime));
+		using (XmlWriter xw = XmlWriter.Create (champFileSavePath, _xwSettings)) {
+			experiment.SavePopulation (xw, new NeatGenome[] { _ea.CurrentChampGenome });
+		}
+		DateTime endTime = DateTime.Now;
+		Utility.Log ("Total time elapsed: " + (endTime - startTime));
 
-        System.IO.StreamReader stream = new System.IO.StreamReader(popFileSavePath);
+		System.IO.StreamReader stream = new System.IO.StreamReader (popFileSavePath);
        
 
       
-        EARunning = false;        
+		EARunning = false;        
         
-    }
+	}
 
-    public void StopEA()
-    {
+	public void StopEA ()
+	{
 
-        if (_ea != null && _ea.RunState == SharpNeat.Core.RunState.Running)
-        {
-            _ea.Stop();
-        }
-    }
+		if (_ea != null && _ea.RunState == SharpNeat.Core.RunState.Running) {
+			_ea.Stop ();
+		}
+	}
 
-    public void Evaluate(IBlackBox box)
-    {
-        GameObject obj = Instantiate(Unit, Unit.transform.position, Unit.transform.rotation) as GameObject;
-        UnitController controller = obj.GetComponent<UnitController>();
+	public void Evaluate (IBlackBox box)
+	{
+		GameObject obj = Instantiate (Unit, Unit.transform.position, Unit.transform.rotation) as GameObject;
+		UnitController controller = obj.GetComponent<UnitController> ();
 
-        ControllerMap.Add(box, controller);
+		ControllerMap.Add (box, controller);
 
-        controller.Activate(box);
-    }
+		controller.Activate (box);
+	}
 
-    public void StopEvaluation(IBlackBox box)
-    {
-        UnitController ct = ControllerMap[box];
+	public void StopEvaluation (IBlackBox box)
+	{
+		UnitController ct = ControllerMap [box];
 
-        Destroy(ct.gameObject);
-    }
+		Destroy (ct.gameObject);
+	}
 
-    public void RunBest()
-    {
-        Time.timeScale = 1;
+	public void RunBest ()
+	{
+		Time.timeScale = 1;
 
-        NeatGenome genome = null;
-
-
-        // Try to load the genome from the XML document.
-        try
-        {
-            using (XmlReader xr = XmlReader.Create(champFileSavePath))
-                genome = NeatGenomeXmlIO.ReadCompleteGenomeList(xr, false, (NeatGenomeFactory)experiment.CreateGenomeFactory())[0];
+		NeatGenome genome = null;
 
 
-        }
-        catch (Exception e1)
-        {
-            // print(champFileLoadPath + " Error loading genome from file!\nLoading aborted.\n"
-            //						  + e1.Message + "\nJoe: " + champFileLoadPath);
-            return;
-        }
+		// Try to load the genome from the XML document.
+		try {
+			using (XmlReader xr = XmlReader.Create (champFileSavePath))
+				genome = NeatGenomeXmlIO.ReadCompleteGenomeList (xr, false, (NeatGenomeFactory)experiment.CreateGenomeFactory ()) [0];
 
-        // Get a genome decoder that can convert genomes to phenomes.
-        var genomeDecoder = experiment.CreateGenomeDecoder();
 
-        // Decode the genome into a phenome (neural network).
-        var phenome = genomeDecoder.Decode(genome);
+		} catch (Exception e1) {
+			// print(champFileLoadPath + " Error loading genome from file!\nLoading aborted.\n"
+			//						  + e1.Message + "\nJoe: " + champFileLoadPath);
+			return;
+		}
 
-        GameObject obj = Instantiate(Unit, Unit.transform.position, Unit.transform.rotation) as GameObject;
-        UnitController controller = obj.GetComponent<UnitController>();
+		// Get a genome decoder that can convert genomes to phenomes.
+		var genomeDecoder = experiment.CreateGenomeDecoder ();
 
-        ControllerMap.Add(phenome, controller);
+		// Decode the genome into a phenome (neural network).
+		var phenome = genomeDecoder.Decode (genome);
 
-        controller.Activate(phenome);
-    }
+		GameObject obj = Instantiate (Unit, Unit.transform.position, Unit.transform.rotation) as GameObject;
+		UnitController controller = obj.GetComponent<UnitController> ();
 
-    public float GetFitness(IBlackBox box)
-    {
-        if (ControllerMap.ContainsKey(box))
-        {
-            return ControllerMap[box].GetFitness();
-        }
-        return 0;
-    }
+		ControllerMap.Add (phenome, controller);
 
-    void OnGUI()
-    {
-        if (GUI.Button(new Rect(10, 10, 100, 40), "Start EA"))
-        {
-            StartEA();
-        }
-        if (GUI.Button(new Rect(10, 60, 100, 40), "Stop EA"))
-        {
-            StopEA();
-        }
-        if (GUI.Button(new Rect(10, 110, 100, 40), "Run best"))
-        {
-            RunBest();
-        }
+		controller.Activate (phenome);
+	}
 
-        GUI.Button(new Rect(10, Screen.height - 70, 100, 60), string.Format("Generation: {0}\nFitness: {1:0.00}", Generation, Fitness));
-    }
+	public float GetFitness (IBlackBox box)
+	{
+		if (ControllerMap.ContainsKey (box)) {
+			return ControllerMap [box].GetFitness ();
+		}
+		return 0;
+	}
+
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (10, 10, 100, 40), "Start EA")) {
+			StartEA ();
+		}
+		if (GUI.Button (new Rect (10, 60, 100, 40), "Stop EA")) {
+			StopEA ();
+		}
+		if (GUI.Button (new Rect (10, 110, 100, 40), "Run best")) {
+			RunBest ();
+		}
+
+		GUI.Button (new Rect (10, Screen.height - 70, 100, 60), string.Format ("Generation: {0}\nFitness: {1:0.00}", Generation, Fitness));
+	}
 }
